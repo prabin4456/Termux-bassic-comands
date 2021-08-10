d8ad0140387f6224f1b11edb08f69f9661f15b92
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-10 08:27:02 +0545

    Update programming language.py

--- programming language.py
+++ programming language.py
@@ -10,3 +10,39 @@ js
 SQL
 PHP
 R programming
+Bootstrap
+Data science
+Excel
+Statistics
+Artificial Intelligence
+Machine Learning
+Go
+AppML
+Accessibility
+Asp
+SciPy
+Pandas
+Numpy
+Matpalotlib
+GIT
+Colors
+Syber Security
+Raspberry Pi
+Canvas
+CVG
+Graphics
+RWD
+Icons
+Sass
+XML
+W3.JS
+Angularjs
+Json
+React
+NodeJs
+MYSQL
+
+
+
+
+

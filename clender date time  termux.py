08553cc7e792028304d00af5b743002dc9174959
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-08-31 16:08:42 +0545

    Add files via upload

---
+++ clender date time  termux.py
@@ -0,0 +1,17 @@
+Termux command
+$ cal -1
+>clender hen termux ma
+$ cal -y
+>years ko clender hen
+$ date +%d
+>31
+$ date +%D
+>31/07/21
+$ date +%Y
+>2021
+$ cal
+> show this mtoth clender
+$ date
+> show date and time >Tue/Aug/31 14:10:37
+$ history
+> tuday  all run pkg show termux history

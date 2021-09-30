1c4c7d65eadc479028dbbcc5e10bb8d209515ca8
Prabin <76979813+prabin4456@users.noreply.github.com>
2021-09-30 11:06:01 +0545

    Update bassic comand termux.py

--- bassic comand termux.py
+++ bassic comand termux.py
@@ -1,20 +1,23 @@
-ls
-apt list
-pwd
-pkg install nano
-nano
-pkg install sudo
-pkg install php
-exit
-cd .. back k liye
-cd Download file mai jane k liye
-banner
-termux-setup-storage
-mv filename.txt /sdcard
-cat filename.txt
-pkg install hydra
-pkg install mc
-mc
-rm file name termux remove delete file
-tourch .pks.py =>create new file 
-ls -a =>show hide file 
+$ ls
+$ apt list
+$ pwd
+$ pkg install nano
+$ nano
+$ nano name.py =>termux mai coding k liye
+& exit =termux apk ko cloge band karne k liye
+& cd .. back k liye
+$ cd Download =>file mai jane k liye
+$ banner
+$ termux-setup-storage => sd card storage parmition enable
+$ mv filename.txt /sdcard =>Termux file Move k Liye
+$ cp name.py /sdcard =>name.py file sdcard copy k liye
+$ cat filename.txt =>file code dekane k liye
+$ pkg install mc =>install pkg all dhekhane k liye
+$ mc =>install pkg all dhekhane k liye tipe mc 
+$ rm name.py =>file name termux remove delete file
+$ tourch .pks.py =>create new file 
+$ ls -a =>show hide file termux
+
+$ pkg install hydra
+$ pkg install sudo
+$ pkg install php
